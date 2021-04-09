ff2b3aae8676bf900c39aa6229755ce3715ef1ca
roll <eskarev@gmail.com>
2021-04-09 10:49:37 +0300

    Removed Form component

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,8 @@
 import { render } from './render'
 import { Report } from './components/Report'
-import { Form } from './components/Form'
 import spec from './spec.json'
 require('./styles/base.css')
 
 // TODO: consider droping this duplication for v3
-export default { render, Report, Form, spec }
-export { render, Report, Form, spec }
+export default { render, Report, spec }
+export { render, Report, spec }
