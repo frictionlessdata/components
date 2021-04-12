e95d0f0e9dcf8ed8592285d0ead5df21533e7616
roll <eskarev@gmail.com>
2021-04-12 09:51:34 +0300

    Updated data/report.json

--- test/components/ReportTask.tsx
+++ test/components/ReportTask.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
 import { render, screen } from '@testing-library/react'
 import { ReportTask } from '../../src/components/ReportTask'
-import report from '../../data/report-test.json'
+import report from '../../data/report.json'
 
 // Tests
 
