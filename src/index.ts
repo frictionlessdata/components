82f4cad2775aa092a3323604a58e1fad4b240aae
roll <eskarev@gmail.com>
2021-04-12 11:17:16 +0300

    Removed legacy export

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,3 @@
-// Legacy
-// TODO: remove this
-
-import { render } from './render'
-import { Report } from './components/Report'
-export default { render, Report }
-export { render, Report }
-
 // Styles
 
 require('./styles/base.css')
