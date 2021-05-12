6f3163c9ebd0b018491fff9d2b0d064f6a31db17
roll <eskarev@gmail.com>
2021-05-12 11:45:14 +0200

    Fixed dist

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ const DEBUG = process.env.DEBUG || false
 // Base
 
 const webpackConfig = {
-  entry: ['./src/index.ts', './src/styles/base.css'],
+  entry: ['./src/index.ts', './src/styles/index.ts'],
   output: {
     path: path.resolve(__dirname, './dist'),
     publicPath: '/dist/',
