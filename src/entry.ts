3942a45165aaedf519187133d6d3ee3fee9aa902
roll <eskarev@gmail.com>
2021-05-15 18:33:38 +0200

    Fixed workflow export

--- src/entry.ts
+++ src/entry.ts
@@ -1,5 +1,6 @@
 // TODO: it's a hack for webpack
 require('./styles/report.css')
 const { Report } = require('./components/Report')
+const { Workflow } = require('./components/Workflow')
 const { render } = require('./render')
-export { Report, render }
+export { Report, Workflow, render }
