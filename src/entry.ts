fd2ba69444c921d3684b4185b0a807e89ffe5945
roll <eskarev@gmail.com>
2021-05-17 17:41:41 +0200

    Added workflow webpack

--- src/entry.ts
+++ src/entry.ts
@@ -1,5 +1,6 @@
 // TODO: it's a hack for webpack
 require('./styles/report.css')
+require('./styles/workflow.css')
 const { Report } = require('./components/Report')
 const { Workflow } = require('./components/Workflow')
 const { render } = require('./render')
