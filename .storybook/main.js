894963b78c47992579d8900a856a824461ce705b
roll <eskarev@gmail.com>
2021-08-01 09:54:24 +0300

    Fixed gtag

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,10 +1,6 @@
 module.exports = {
   stories: ['../docs/**/*.stories.mdx', '../docs/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: [
-    '@storybook/addon-links',
-    '@storybook/addon-essentials',
-    '@storybook/addon-google-analytics',
-  ],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
   webpackFinal: async (config, { configType }) => {
     config.node = { ...config.node, fs: 'empty' }
     return config
--- .storybook/manager.js
+++ .storybook/manager.js
@@ -1,7 +1,6 @@
 import { addons } from '@storybook/addons'
 import theme from './theme'
 
-window.STORYBOOK_GA_ID = 'G-W9VGXN0NP4'
 addons.setConfig({
   panelPosition: 'right',
   theme,
