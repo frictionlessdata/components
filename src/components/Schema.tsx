3dcea2d0cf28e616e6cd719ce710a722e64d6b4a
roll <eskarev@gmail.com>
2021-10-16 18:26:16 +0300

    Fixed typescript compilation

--- src/components/Schema.tsx
+++ src/components/Schema.tsx
@@ -34,6 +34,7 @@ export function Schema(props: ISchemaProps) {
       setColumns(columns)
       setMetadata(metadata)
     } catch (error) {
+      // @ts-ignore
       setError(error)
       setLoading(false)
     }
